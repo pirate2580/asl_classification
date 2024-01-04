a88c392963f58e0e056aad2f099ebe4d8e7f49ea
Naoroj Farhan <farhannaoroj@gmail.com>
2024-01-03 21:51:32 -0500

    Add a custom dataset class in order to encode labels and bounding box

--- datapreprocess.py
+++ datapreprocess.py
@@ -1,63 +1,115 @@
-import cv2
+# Importing Libraries
 import os
-import numpy as np
+import torch
+from torch.utils.data import DataLoader, random_split, Dataset
+import torchvision.transforms as transforms
+from torchvision.datasets import ImageFolder
+from PIL import Image
 
-# There are 29 classes for the hand signs that we want to classify correctly in real time from our camera
-
-# creating a map corresponding to the labels
+ # creating a map corresponding to the labels
 alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+
 labels = {alphabet[i]: i for i in range(len(alphabet))}
 labels['del'] = 26
 labels['nothing'] = 27
 labels['space'] = 28
 
+# custom Dataset in order to include all 29 class labels and the bounding box for each of them
+class CustomDataset(Dataset):
+  def __init__(self, root_dir, transform=None):
+      self.root_dir = root_dir
+      self.transform = transform
+      self.class_names = sorted(os.listdir(root_dir))     # list of strings containing the names of all folders in root_dir
+      self.data = self._load_data()                       # calls own load_data method
 
-def folder_to_numpy(folder_path: str):
-  """
-  This function goes inside the images in each training set folder and converts images into 4D
-  numpy array of dimensions (batch_size, height, width, channels)
+  def _load_data(self):
+      data = []                                                  # data is a list of dictionary for each image containing imagepath,
+
+      for class_name in self.class_names:                        # self.class_names is a list of the names of all the folders containing classified hand images
+          class_dir = os.path.join(self.root_dir, class_name)    # specific class folder
+          for file_name in os.listdir(class_dir):                # file name in a given class folder
+              image_path = os.path.join(class_dir, file_name)
+              data.append({
+                  'image_path': image_path,
+                  'truth_labels': self.get_label(class_name),              # 29
+                  'bounding_box': self.get_bounding_box_info(),  # 4 
+              })
+      return data
   
-  labels are correspondingly given
+  def get_label(self, class_name):
+      """
+      Return a list for the ground truth labels
+      """
+      truth_label = [0] * 29
+      truth_label[labels[class_name]] = 1    #TODO: check for weird float or int issue
+      return truth_label
+  
+
+  def get_bounding_box_info(self):
+      """
+      Return bounding box information (e.g., coordinates)
+      Since all the images are closely cropped, lets set bx, by, bw, bh
+
+      Note bx and by encode the center and bw and bh are set to 1 since its closely cropped
+      """
+      return [0.5, 0.5, 1, 1] #TODO: check this later for random float nonsense
+
+  def __len__(self):
+      return len(self.data)
+
+  def __getitem__(self, idx):
+      sample = self.data[idx]
+      image = Image.open(sample['image_path']).convert('RGB')
+
+      if self.transform:
+          image = self.transform(image)
+
+      return {
+          'image': image,
+          'label': sample['class_name'],
+          'bounding_box': sample['bounding_box'],
+      }
+
+transform = transforms.Compose([
+    transforms.Resize((224, 224)),
+    transforms.ToTensor(),
+])
+
+# Create dataset instance
+dataset = CustomDataset(root_dir='asl_alphabet_train', transform=transform)
+# Create DataLoader
+data_loader = DataLoader(dataset, batch_size=32, shuffle=True)
+
+
+train_transform = transforms.Compose([
+  
+  # Resize to 224x224, since images are 200x200, it just resizes larger while 
+  # maintaining aspect ratio, thus, the image stays closely cropped, yolo works on images that satisfies : 32|size
+  transforms.Resize(224),      
+  # 50% chance of horizontal flip so that the model doesn't overfit to a single horizontal orientation
+  transforms.RandomHorizontalFlip(),
+  # transform image to tensor for training
+  transforms.ToTensor()
+])
+
+test_transform = transforms.Compose([
+    transforms.Resize(224),
+    transforms.ToTensor(),
+])
+
+def load_data(data_dir):
   """
-  image_list = []
-  labels_list = []
+  Takes string representing data directory and loads the training and test set from it
+  """
+  dataset = dataset = ImageFolder(root=data_dir)
+  train_dataset, test_dataset = random_split(dataset, [0.8, 0.2])
 
-  cnt = 0
-
-  for class_folder in os.listdir(folder_path):
-    if class_folder != ".DS_Store":
-
-      #counts the ith letter being preprocessed and prints which letter it is
-      cnt += 1
-      print(f"{class_folder} {cnt}")
-
-      for filename in os.listdir(folder_path + '/' + class_folder):
-        if filename.endswith(".jpg") or filename.endswith(".jpeg"):
-              # Read each image using OpenCV
-              # print(folder_path + '/' + class_folder)
-              image_path = os.path.join(folder_path + '/' + class_folder, filename)
-              image = cv2.imread(image_path)
-
-              # Check if the image is loaded successfully
-              if image is not None:
-                  # Convert the image to a NumPy array
-                  image_array = np.array(image)
-
-                  # Append the image array to the image
-                  image_list.append(image_array)
-                  labels_list.append(labels[class_folder])
-
-  # Stack the image arrays to create a 3D array
-  image_stack = np.stack(image_list, axis = 0)
-  labels_stack = np.stack(labels_list, axis = 0)
-
-  return image_stack, labels_stack
-
-folder_path = "/Users/naorojfarhan/Desktop/self_study/coding_projects/ML_projects/asl_classification/asl_alphabet_train"
-images_4d_array, truth_labels = folder_to_numpy(folder_path)
-
-# save the input and ground truth labels to npy file
-np.save('train_x.npy', images_4d_array)
-np.save('train_y.npy', truth_labels)
-
-print("Shape of the 4D NumPy array:", images_4d_array.shape)
+  # transformation applied to each dataset
+  train_dataset.dataset.transform = train_transform
+  test_dataset.dataset.transform = test_transform
+  # Defining Training and Testing DataLoaders
+    # You can use batch size as a hyperparameter if needed?
+  train_loader = DataLoader(train_dataset, batch_size=32, shuffle=True)
+  test_loader = DataLoader(test_dataset, batch_size=32, shuffle=True)
+  
+  return train_loader, test_loader
